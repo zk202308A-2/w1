92cb115dd3756da7b9cff42e3fb938503d8c7329
inhawinter <inhawinter@gmail.com>
2025-06-24 14:44:24 +0900

    asdfasdff

--- src/cart.js
+++ src/cart.js
@@ -3,17 +3,24 @@ let cart = []
 
 export function addCart(menu) {
 
-    console.log("add")
+    console.log("--------------------1")
+    console.log(menu)
     
-
     const targetMenu =cart.filter(obj => obj.mno === menu.mno)
+    console.log("--------------------2")
+    console.log(targetMenu)
 
     if(targetMenu){
+        console.log("--------------------3-1")
+        
         targetMenu.qty = targetMenu.qty +1
     }else {
+        console.log("--------------------3-2")
         menu.qty = 1
         cart = [...cart, menu]
     }
+    
+    console.log(cart)
 
 }
 export function removeCart(mno) {
